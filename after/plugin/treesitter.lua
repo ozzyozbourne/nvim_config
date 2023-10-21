7e13ce9aef79cc20626ba436172d513c9f11f285
ozzy ozbourne <khanosaid726@gmail.com>
2023-10-21 11:12:00 +0530

    fixed java lsp issue

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -13,7 +13,6 @@ require 'nvim-treesitter.configs'.setup {
         "dockerfile",
         "elixir",
         "erlang",
-        "elm",
         "gitignore",
         "go",
         "gosum",
@@ -31,18 +30,12 @@ require 'nvim-treesitter.configs'.setup {
         "yaml",
         "zig",
         "rust",
-        "c_sharp",
         "haskell",
         "python",
-        "fsh",
-        "julia",
-        "scala" },
+    },
 
-    -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
     auto_install = true,
 
     ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
@@ -50,8 +43,6 @@ require 'nvim-treesitter.configs'.setup {
 
     highlight = {
         enable = true,
-
-
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
