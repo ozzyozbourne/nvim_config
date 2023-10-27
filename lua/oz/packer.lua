5fcfc6525a0a87e63d8f7c2e385d58c7fe44cae6
ozzy ozbourne <khanosaid726@gmail.com>
2023-10-27 22:09:30 +0530

    updated config

--- lua/oz/packer.lua
+++ lua/oz/packer.lua
@@ -6,16 +6,12 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
-
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
         requires = { { 'nvim-lua/plenary.nvim' } }
     }
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-
-    vim.cmd('colorscheme rose-pine')
-
     use { 'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons' }
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
