2bca42667119e0ce261c52b1cf7893fcc290053f
ozzy ozbourne <khanosaid726@gmail.com>
2023-11-09 21:09:26 +0530

    updated config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,5 +1,6 @@
 vim.keymap.set('n', [[<leader>bl]], ':BufferLinePick<CR>')
 vim.keymap.set('n', [[<leader>bc]], vim.cmd.BufferLineCloseOthers)
+vim.keymap.set('n', [[C-g]], vim.cmd.BufferLineCycleNext)
 
 require("bufferline").setup {
     options = {
